960aa48c2b4cc4d11abfb6966010ca3221f75a6c
Muhammad Rizki Juanda <muhammadrizkijuanda@gmail.com>
2016-01-10 23:56:57 +0700

    no message

---
+++ 26-Final/Tumpukan.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+/**
+ * Write a description of class Tumpukan here.
+ * 
+ * @author (M Rizki Juanda) 
+ * @version (a version number or a date)
+ */
+public class Tumpukan
+{
+ 
+    private Node atas;
+    private Node bawah;
+    
+    public Tumpukan(){
+    atas=null; 
+    }
+    
+    public void tumpuk (BigInteger data){
+       
+    
+    }
+    
+    public BigInteger ambil(){
+    return null;
+    }
+    
+       public BigInteger atas(){
+    return null;
+    }
+    
+        public BigInteger bawah(){
+    return null;
+    }
+}
